91408957ad712f3db1c40a94e3b604a04a897564
FaresAkmjoud <faresalmjoud@gmail.com>
2024-10-21 21:52:16 +0300

    HelperBot
    HelperBot

---
+++ config[1].js
@@ -0,0 +1,23 @@
+const config = require("./config.js");
+const Discord = require("discord.js");
+//exports.token = "";
+//exports.mongodb = "";
+
+
+exports.owners = ["939561307173191691","","",""];
+
+// colors
+exports.ImageColors = "https://media.discordapp.net/attachments/1084557877802438717/1088531232524079214/IMG_8908.jpg" // صوره الالوان
+exports.Text = "colors list" // كلام على الصوره
+exports.Textcolor = "#f2f5f4" // لون النص لي على الصوره
+
+
+
+
+exports.status = 'idle';
+
+exports.activity = {
+  name: 'Type !help ',
+  type: 'STREAMING',
+  url: 'https://www.twitch.tv/sfsf13'
+};
